bf6173da694984d6ab7acc3ab5a7f7d1acf744b2
zalex <zalex@zalex.com.ua>
2020-05-04 10:52:45 +0300

    increase timeout to 5 sec

--- src/Services/MPD/MPDClientFactory.php
+++ src/Services/MPD/MPDClientFactory.php
@@ -10,12 +10,22 @@ use Kolbasyatin\MPD\MPD\Answers\SimpleAnswer;
 use Kolbasyatin\MPD\MPD\MPDClient;
 use Kolbasyatin\MPD\MPD\MPDConnection;
 
+/**
+ * Class MPDClientFactory
+ * @package App\Services\MPD
+ */
 class MPDClientFactory
 {
+    /**
+     * @param array $mpdOptions
+     * @return MPDClient
+     * @throws \Kolbasyatin\MPD\MPD\Exceptions\MPDConnectionException
+     */
     public static function createClient(array $mpdOptions)
     {
         ['host' => $host, 'port' => $port, 'password' => $password] = $mpdOptions;
         $connection = new MPDConnection(sprintf('%s:%d', $host, $port), $password);
+        $connection->setSocketTimeOut(5);
 
         return new MPDClient($connection, new SimpleAnswer());
     }
